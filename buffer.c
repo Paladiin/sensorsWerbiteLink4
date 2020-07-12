7e89d89c622b677c0a228f6227a79bda77ac990a
Paladiin <92659828+Paladiin@users.noreply.github.com>
2020-07-12 07:20:04 +0000

    Version 0.3.1

--- buffer.c
+++ buffer.c
@@ -9,3 +9,8 @@ buffer_create(size_t size)
     struct buffer *buf = malloc(sizeof(*buf));
     if (!buf)
         return NULL;
+
+    buf->start = malloc(size);
+    if (!buf->start) {
+        free(buf);
+        retu

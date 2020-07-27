6bd0d053d3392b72a3acd713985937f273cb34d8
Paladiin <92659828+Paladiin@users.noreply.github.com>
2020-07-27 19:29:24 +0000

    v1.1.10

--- examples/demo.lua
+++ examples/demo.lua
@@ -3,4 +3,8 @@ socket = require "ssocket"
 tcpsock = socket.tcp()
 ok, err = tcpsock:connect("www.google.com", 80)
 if err then
-  print
+  print(err)
+end
+tcpsock:write("GET / HTTP/1.1\r\n")
+tcpsock:write("\r\n")
+tcpsock

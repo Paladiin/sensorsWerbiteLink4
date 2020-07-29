9e5ae1da005701d3c802f75ec12953b0c8a43b12
Paladiin <92659828+Paladiin@users.noreply.github.com>
2020-07-29 19:38:00 +0000

    v1.3.8

--- examples/echoserver-unix.lua
+++ examples/echoserver-unix.lua
@@ -1,4 +1,8 @@
 #!/usr/bin/env lua
 local socket = require "ssocket"
 
-SOCK_PATH = '/tmp/test.soc
+SOCK_PATH = '/tmp/test.sock'
+tcpsock = socket.tcp()
+os.remove(SOCK_PATH)
+local ok, err = tcpsock:bind(SOCK_PATH)
+if err the

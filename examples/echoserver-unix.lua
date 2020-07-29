92fda0d63f6f6be8ec8c0b11ddb8ff1767dcc4f3
Paladiin <92659828+Paladiin@users.noreply.github.com>
2020-07-29 19:47:13 +0000

    Map

--- examples/echoserver-unix.lua
+++ examples/echoserver-unix.lua
@@ -5,4 +5,11 @@ SOCK_PATH = '/tmp/test.sock'
 tcpsock = socket.tcp()
 os.remove(SOCK_PATH)
 local ok, err = tcpsock:bind(SOCK_PATH)
-if err the
+if err then
+  print(err)
+  os.exit()
+end
+tcpsock:listen(5)
+
+addr, err = tcpsock:getsockname()
+prin

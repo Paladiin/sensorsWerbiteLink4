efd219f8f50b5ff50813b1f2615b8dd5752e02e0
Paladiin <92659828+Paladiin@users.noreply.github.com>
2020-09-10 16:11:23 +0000

    fixing lint errors

--- examples/readuntil.lua
+++ examples/readuntil.lua
@@ -2,4 +2,6 @@ local socket = require "ssocket"
 
 local sock = socket.tcp()
 local ok, err = sock:connect('www.baidu.com', 80)
-sock:write("GET /robots.txt HT
+sock:write("GET /robots.txt HTTP/1.1\r\n")
+sock:write("Host: www.baidu.com\r\n")
+sock:write("Connection: Close\r

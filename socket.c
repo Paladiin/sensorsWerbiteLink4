92d50d3ecbf9aea965962e5b16e24af27fc48943
Paladiin <92659828+Paladiin@users.noreply.github.com>
2020-09-18 19:23:49 +0000

    Script update after bundler exec jekyll build

--- socket.c
+++ socket.c
@@ -56,4 +56,6 @@ struct sockobj {
 #define ERROR_REFUSED   "Connection refused"
 
 /* Options */
-#define OPT_TCP_NODELAY   "
+#define OPT_TCP_NODELAY   "tcp_nodelay"
+#define OPT_TCP_KEEPALIVE "tcp_keepalive"
+#define OPT_TCP_REUSEADDR "tcp_

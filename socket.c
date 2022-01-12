d12e143d2824d90456dfbc4da71e4fb6d16b8e71
Paladiin <92659828+Paladiin@users.noreply.github.com>
2022-01-12 22:14:12 +0000

    debug statements

--- socket.c
+++ socket.c
@@ -843,4 +843,7 @@ sockobj_tostring(lua_State * L)
 {
     struct sockobj *s = getsockobj(L);
     assert(lua_getmetatable(L, -1));
-    luaL_getmetatable(L, TCPSOCK_TYPENAME)
+    luaL_getmetatable(L, TCPSOCK_TYPENAME);
+    if (lua_rawequal(L, -1, -2)) {
+        lua_pop(L, 2);
+        lua_pushfstring(L, "

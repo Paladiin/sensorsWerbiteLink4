f30e00aef6169bab81058cfa6630bb30d8a8ecd2
Paladiin <92659828+Paladiin@users.noreply.github.com>
2022-02-19 18:07:34 +0000

    :arrow_up: Upgrade dependencies

--- socket.c
+++ socket.c
@@ -903,4 +903,7 @@ tcpsock_connect(lua_State * L)
     if (__sockobj_getaddrfromarg(L, s, SAS2SA(&addr), &len, 1)) {
         return 2;
     }
-    if (__sockobj_createsocket(L, s, SO
+    if (__sockobj_createsocket(L, s, SOCK_STREAM) == -1) {
+        return 2;
+    }
+    if (__sockobj_connect(L, s, SAS2SA(&addr)

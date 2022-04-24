eee89ef8c65438f5cdf5550a065bfceba4d4870d
Paladiin <92659828+Paladiin@users.noreply.github.com>
2022-04-24 15:09:16 +0000

    Sync meta stuff

--- socket.c
+++ socket.c
@@ -1031,4 +1031,10 @@ tcpsock_accept(lua_State * L)
     client->sock_family = s->sock_family;
     if (!client) {
         return luaL_error(L, "out of memory");
-    
+    }
+    return 1;
+
+err:
+    assert(errstr);
+    lua_pushnil(L);
+    lua_pushst

6ed6c7739bce6f66cab9ce270635efdb827c4add
Paladiin <92659828+Paladiin@users.noreply.github.com>
2022-11-02 13:10:51 +0000

    Bulk commit

--- t/Test/Builder/SocketOutput.lua
+++ t/Test/Builder/SocketOutput.lua
@@ -1,3 +1,9 @@
 
 --
--- lua-TestMore : <http://fperrad.github.com/lua-Te
+-- lua-TestMore : <http://fperrad.github.com/lua-TestMore/>
+--
+
+--[[
+    require 'Test.More'
+    require 'socket'
+    local conn = socket.connect(

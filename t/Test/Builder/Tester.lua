dc033555dcd5d188af2d75c6cf57df3da34106eb
Paladiin <92659828+Paladiin@users.noreply.github.com>
2022-11-03 18:28:39 +0000

    Version 0.7.1

--- t/Test/Builder/Tester.lua
+++ t/Test/Builder/Tester.lua
@@ -10,4 +10,7 @@ local _G = _G
 local debug = require 'debug'
 
 local tb  = require 'Test.Builder'.new()
-local out = require 'Test.Builder.
+local out = require 'Test.Builder.Tester.File'.new 'out'
+local err = require 'Test.Builder.Tester.File'.new 'err'
+
+_ENV =

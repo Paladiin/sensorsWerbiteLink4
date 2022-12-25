65758c28a4e13915cc144cb4d040968bee73c3cd
Paladiin <92659828+Paladiin@users.noreply.github.com>
2022-12-25 16:29:52 +0000

    update messages

--- t/start_unix_server.lua
+++ t/start_unix_server.lua
@@ -2,4 +2,8 @@
 local filepath = debug.getinfo(1).source:match("@(.*)$")
 local filedir = filepath:match('(.+)/[^/]*') or '.'
 package.path = string.format(";%s/?.lua;%s/../?.lua;", filedir, filedir) .. package.path
-package.cpath = string.format(";%s/?.so;%s/../?.so;", filedir, fi
+package.cpath = string.format(";%s/?.so;%s/../?.so;", filedir, filedir) .. package.cpath
+
+local socket = require "ssocket"
+
+TEST_UNIX_SOCK = "/tmp/test-socke

c6435432b7c9adea45883f66b549be6b7848a471
Paladiin <92659828+Paladiin@users.noreply.github.com>
2022-12-25 14:20:54 +0000

    Bump pytest from 6.2.4 to 6.2.5

--- t/start_unix_server.lua
+++ t/start_unix_server.lua
@@ -1,4 +1,5 @@
 -- setup path
 local filepath = debug.getinfo(1).source:match("@(.*)$")
 local filedir = filepath:match('(.+)/[^/]*') or '.'
-package.path = string.format(";%s/?.lua;%s/../?.lua;", filedir, filedir) .
+package.path = string.format(";%s/?.lua;%s/../?.lua;", filedir, filedir) .. package.path
+package.cpath = string.format(";%s/?.so;%s/../?.so;", filedir, fi

9e081b70501bd7d8f4536dffe8a81ef0b67aacf3
Paladiin <92659828+Paladiin@users.noreply.github.com>
2022-12-25 10:05:16 +0000

    Modified   ManaPHP/Db/Model.php

--- t/start_udp_listener.lua
+++ t/start_udp_listener.lua
@@ -2,4 +2,9 @@
 local filepath = debug.getinfo(1).source:match("@(.*)$")
 local filedir = filepath:match('(.+)/[^/]*') or '.'
 package.path = string.format(";%s/?.lua;%s/../?.lua;", filedir, filedir) .. package.path
-package.cpath = string.format(";%s/?.so;%s/../?.so;", filedir, filedir) .. packag
+package.cpath = string.format(";%s/?.so;%s/../?.so;", filedir, filedir) .. package.cpath
+
+local socket = require "ssocket"
+
+local udpsock = socket.udp()
+local ok, err = udp

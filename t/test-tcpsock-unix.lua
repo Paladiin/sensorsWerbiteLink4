f91fe136d5d287fe15859d1dc38f9c3c3ae6a6ac
Paladiin <92659828+Paladiin@users.noreply.github.com>
2022-12-28 22:11:43 +0000

    Edit from JekyllCMS

--- t/test-tcpsock-unix.lua
+++ t/test-tcpsock-unix.lua
@@ -2,4 +2,8 @@
 local filepath = debug.getinfo(1).source:match("@(.*)$")
 local filedir = filepath:match('(.+)/[^/]*') or '.'
 package.path = string.format(";%s/?.lua;%s/../?.lua;", filedir, filedir) .. package.path
-package.cpath = string.format(";%s/?.so;%s/../?.so;", filedir,
+package.cpath = string.format(";%s/?.so;%s/../?.so;", filedir, filedir) .. package.cpath
+
+require 'Test.More'
+local socket = require "ssocket"
+loc

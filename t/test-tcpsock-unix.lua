772e057368ba2447e0c5e4ddfee4b3fc6d3c6ae4
Paladiin <92659828+Paladiin@users.noreply.github.com>
2022-12-28 09:17:26 +0000

    Remove broken test

--- t/test-tcpsock-unix.lua
+++ t/test-tcpsock-unix.lua
@@ -1,3 +1,4 @@
 -- setup path
 local filepath = debug.getinfo(1).source:match("@(.*)$")
-local filedir = filepath:match('
+local filedir = filepath:match('(.+)/[^/]*') or '.'
+package.path = string.format(";%s/?.lua;%s/../?.lua;", filedir, filedi

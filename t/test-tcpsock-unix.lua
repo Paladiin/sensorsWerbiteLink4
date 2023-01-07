90a168f034e0f81bb6b16977a5d555fde95285ce
Paladiin <92659828+Paladiin@users.noreply.github.com>
2023-01-07 08:57:03 +0000

    Updated javadocs

--- t/test-tcpsock-unix.lua
+++ t/test-tcpsock-unix.lua
@@ -29,4 +29,7 @@ tcpsock:close()
 
 -- 3. connect
 os.execute(string.format("%s %s/start_unix_server.lua &>/dev/null &", lua_bin, filedir))
-os.execute("sleep 1") -- make sure service i
+os.execute("sleep 1") -- make sure service is on
+local tcpsock = socket.tcp()
+local ok, err = tcpsock:connect(TEST_UNIX_SOCK)
+is(ok, tr

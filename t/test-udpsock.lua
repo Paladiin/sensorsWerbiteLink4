d93b4b4d53dd311703805377f40bb5935d7e1602
Paladiin <92659828+Paladiin@users.noreply.github.com>
2023-02-25 17:29:08 +0000

    Update .gitconfig

--- t/test-udpsock.lua
+++ t/test-udpsock.lua
@@ -37,3 +37,17 @@ sendsock:connect('localhost', 8888)
 is(sendsock:send(packet_data), true)
 data, err = recvsock:recv(8192)
 is(data, packet_data)
+
+-- 3. sendto/recvfrom
+local sendsock = socket.udp()
+ok, err = sendsock:sendto(packet_data, 'localhost', 8888)
+data, addr, err = recvsock:recvfrom(8192)
+is(data, packet_data)
+is(addr[1], '127.0.0.1')
+type_ok(addr[2], "number")
+
+-- 4. send big data
+longstr = string_repeat("a",165507)
+ok, err = sendsock:sendto(longstr, "8.8.8.8", 53)
+is(ok, nil)
+is(err, "Message too long")
---
+++ t/udp_sender.py
@@ -0,0 +1,5 @@
+import socket
+
+# addressing information of target
+IPADDR = 'localhost'
+PORTNUM = 8888

bbfbb5ac3a92900c9d5011ff589747b9b5772b49
Paladiin <92659828+Paladiin@users.noreply.github.com>
2023-02-25 20:12:52 +0000

    x86, x32, ptrace: Remove PTRACE_ARCH_PRCTL for x32

--- t/udp_sender.py
+++ t/udp_sender.py
@@ -3,3 +3,7 @@ import socket
 # addressing information of target
 IPADDR = 'localhost'
 PORTNUM = 8888
+
+# enter the data content of the UDP packet as hex
+PACKETDATA = "Hello World! A udp packet."
+#PAC
